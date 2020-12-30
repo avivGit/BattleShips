c05e673ffac34c7ce538d706c5c627eb5277594d
Aviv Malka <avivm212@gmail.com>
2020-12-30 14:42:40 +0200

    add player1,2 mains

---
+++ test_player1.py
@@ -0,0 +1,10 @@
+from game_player import Player1
+
+
+def main():
+    player = Player1()
+    player.start_playing()
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_player2.py
@@ -0,0 +1,10 @@
+from game_player import Player2
+
+
+def main():
+    player = Player2()
+    player.start_playing()
+
+
+if __name__ == '__main__':
+    main()
